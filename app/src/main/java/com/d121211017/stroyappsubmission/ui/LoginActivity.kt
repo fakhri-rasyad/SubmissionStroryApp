577620884a514c1a94afc278b9d3c79cd65fa437
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-04-29 14:50:02 +0800

    feat: login ui

--- app/src/main/java/com/d121211017/stroyappsubmission/ui/LoginActivity.kt
+++ app/src/main/java/com/d121211017/stroyappsubmission/ui/LoginActivity.kt
@@ -6,10 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.d121211017.stroyappsubmission.R
+import com.d121211017.stroyappsubmission.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
