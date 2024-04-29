eba8e2b9cb4ba3fd3bd145bcd1cfcec1cee74739
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-04-29 13:07:36 +0800

    feat: ViewBinding

---
+++ app/src/main/java/com/d121211017/stroyappsubmission/ui/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.d121211017.stroyappsubmission.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.d121211017.stroyappsubmission.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
