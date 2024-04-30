eb75065860c94711316f9d9978ee6f75306da24b
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-04-30 17:41:22 +0800

    feat: ViewModelFactory

---
+++ app/src/main/java/com/d121211017/stroyappsubmission/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.d121211017.stroyappsubmission.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel(application: Application): ViewModel() {
+}
