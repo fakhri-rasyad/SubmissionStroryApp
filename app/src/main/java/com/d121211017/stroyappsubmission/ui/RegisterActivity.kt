96496b00b53b3f148ac34c2b4247581b0738a4ba
fakhri-rasyad <86504619+fakhri-rasyad@users.noreply.github.com>
2024-05-01 15:05:03 +0800

    feat: Register form input

--- app/src/main/java/com/d121211017/stroyappsubmission/ui/RegisterActivity.kt
+++ app/src/main/java/com/d121211017/stroyappsubmission/ui/RegisterActivity.kt
@@ -1,16 +1,71 @@
 package com.d121211017.stroyappsubmission.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
+import androidx.lifecycle.ViewModelProvider
 import com.d121211017.stroyappsubmission.R
+import com.d121211017.stroyappsubmission.databinding.ActivityRegisterBinding
+import com.d121211017.stroyappsubmission.viewmodel.RegisterViewModel
+import com.d121211017.stroyappsubmission.viewmodel.ViewModelFactory
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityRegisterBinding
+    private lateinit var viewModel: RegisterViewModel
+
+    companion object {
+        private const val TAG = "Register Activity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = getViewModel(this)
+
+        viewModel.isButtonEnabled.observe(this){
+            binding.registerButton.isEnabled = it
+        }
+
+        viewModel.isEmailValid.observe(this){
+            binding.emailInput.error = if(!it) getString(R.string.email_invalid) else null
+        }
+
+        viewModel.isPasswordValid.observe(this){
+            binding.passwordInput.error = if(!it) getString(R.string.password_invalid) else null
+        }
+
+
+        binding.apply {
+            nameInput.addTextChangedListener(
+                onTextChanged = {text, _, _, _ ->
+                    viewModel.nameValidation(text.toString().trim())
+                }
+            )
+
+            emailInput.addTextChangedListener(
+                afterTextChanged = { editable ->
+                    viewModel.emailValidation(editable.toString())
+                }
+            )
+            passwordInput.addTextChangedListener(
+                afterTextChanged = { editable ->
+                    viewModel.passwordValidation(editable.toString())
+                }
+            )
+
+            registerButton.setOnClickListener {
+                viewModel.getData()
+            }
+        }
+    }
+
+    private fun getViewModel(appCompatActivity: AppCompatActivity) : RegisterViewModel {
+        val factory = ViewModelFactory.getInstance(appCompatActivity.application)
+        return ViewModelProvider(appCompatActivity, factory)[RegisterViewModel::class.java]
     }
 }
--- app/src/main/java/com/d121211017/stroyappsubmission/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/d121211017/stroyappsubmission/viewmodel/RegisterViewModel.kt
@@ -2,7 +2,9 @@ package com.d121211017.stroyappsubmission.viewmodel
 
 import android.app.Application
 import android.util.Log
+import android.util.Patterns
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.d121211017.stroyappsubmission.data.remote.entity.SimpleResponse
@@ -19,6 +21,22 @@ class RegisterViewModel(application: Application): ViewModel() {
 
     private val _isLoadingRegistration = MutableLiveData<Boolean>()
     val loadingRegistration : LiveData<Boolean> = _isLoadingRegistration
+
+    private val _isEmailValid = MutableLiveData<Boolean>()
+    val isEmailValid : LiveData<Boolean> = _isEmailValid
+
+    private val _isPasswordValid = MutableLiveData<Boolean>()
+    val isPasswordValid : LiveData<Boolean> = _isPasswordValid
+
+    private val _isButtonEnabled = MediatorLiveData<Boolean>().apply {
+        addSource(_isEmailValid) { value = it && (_isPasswordValid.value ?: false) }
+        addSource(_isPasswordValid) { value = it && (_isEmailValid.value ?: false) }
+    }
+    val isButtonEnabled: LiveData<Boolean> = _isButtonEnabled
+
+    private var name = ""
+    private var email = ""
+    private var password = ""
 
     fun postRegistration(
         name : String,
@@ -45,5 +63,23 @@ class RegisterViewModel(application: Application): ViewModel() {
 
         })
 
+    }
+
+    fun getData(){
+        Log.d(TAG, "$name, $email, $password")
+    }
+
+    fun nameValidation(name: String){
+        this.name = name
+    }
+    fun emailValidation(email: String){
+        _isEmailValid.postValue(Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        this.email = email
+    }
+
+    fun passwordValidation(password : String){
+        if (password.isNotEmpty() && password.length >= 8) _isPasswordValid.postValue(true)
+        else _isPasswordValid.postValue(false)
+        this.password = password
     }
 }
